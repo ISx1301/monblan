cd357148939e0d97afd4a308c0b20365f6b46ff1
DubblerPro <igorshkuro2000@gmail.com>
2026-06-05 16:55:06 +0300

    Registered gallery modules in app bootstrap

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
+import { initCards } from './cards.js'
 import { initDatepickers } from './datepickers.js'
+import { initLayoutSwitcher } from './layout-switcher.js'
 
 export function app() {
+  initCards()
   initDatepickers()
+  initLayoutSwitcher()
 }
